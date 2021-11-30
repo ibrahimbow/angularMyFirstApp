441dc057b7e674de525f6d7a574c6907c0671f6c
ibrahimbow <smartman402@hotmail.com>
2021-11-30 14:57:27 +0100

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileGalleryComponent } from './profile-gallery/profile-gallery.component';
+import { SetupDateComponent } from './setup-date/setup-date.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: ProfileGalleryComponent},
+{path: 'setup-date/:name', component: SetupDateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
+import { NameFilterPipe } from './pipe/name-filter.pipe';
+import { ProfileGalleryComponent } from './profile-gallery/profile-gallery.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { SetupDateComponent } from './setup-date/setup-date.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileGalleryComponent,
+    NameFilterPipe,
+    SetupDateComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    AppRoutingModule,
+    HttpClientModule,
+    
   ],
-  providers: [],
+  providers: [
+
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
